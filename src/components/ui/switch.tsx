cb9ebf95c75decbe3032caa9d7242dd54e0c09e3
Konstantin Mednikov <irondsd@gmail.com>
2026-01-20 20:26:26 -0300

    fix switch dark theme

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -19,7 +19,7 @@ const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
           onChange={(e) => onCheckedChange?.(e.target.checked)}
           {...props}
         />
-        <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700 rtl:peer-checked:after:-translate-x-full"></div>
+        <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-gray-400 rtl:peer-checked:after:-translate-x-full"></div>
       </label>
     );
   },
